4bb828673bfc0104ec8014d86e805ca40c711ee3
Sushant-10-k <sushantjaiswal64@gmail.com>
2026-07-06 14:54:48 +0530

    Except deployment its ready

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res = tavily_search("Best rating holtels in India")
 # print(res)
 
-res = search_flights("Plan a 10 days US trip from India with a budget of 2000 USD")
-print(res)
+# res = search_flights("Plan a 10 days US trip from India with a budget of 2000 USD")
+# print(res)
+
+user_input = input("Enter your travel query: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_thread",
+    )
+
+print("\n\nFinal Itinerary:\n")
+print(response["answer"])
